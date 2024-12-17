8284a4a101d111e3ae54479e7a800c7bf376049c
Om dev <43951418+omdev74@users.noreply.github.com>
2024-12-17 09:16:50 -0500

    added coditonal rendering through tailwind to navbar and made a flaoting pill for the dark mode

--- client/src/components/ModeToggle.tsx
+++ client/src/components/ModeToggle.tsx
@@ -16,11 +16,22 @@ export function ModeToggle() {
         <Button
             variant="ghost"
             size="icon"
+            className="h-9 w-9 sm:h-10 sm:w-10 rounded-full"// Responsive button size
             onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         >
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            {/* Sun Icon */}
+            <Sun
+                className="h-8 w-8 sm:h-5 sm:w-5 rotate-0 scale-100 transition-transform duration-300 ease-in-out
+        dark:-rotate-90 dark:scale-0"
+            />
+            {/* Moon Icon */}
+            <Moon
+                className="absolute h-8 w-8 sm:h-5 sm:w-5 rotate-90 scale-0 transition-transform duration-300 ease-in-out
+        dark:rotate-0 dark:scale-100"
+            />
             <span className="sr-only">Toggle theme</span>
         </Button>
+
+
     )
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,39 +6,73 @@ import { ModeToggle } from "./ModeToggle";
 
 export function Navbar() {
     return (
-        <nav className="bg-background border-b">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between h-16">
-                    <div className="flex">
-                        <h1 className="text-2xl font-bold flex items-center space-x-2">
-                            <Dumbbell className="h-6 w-6 text-primary" /> {/* Adjust size with `h-6 w-6` classes */}
-                            <span><Link to="/">GymBook</Link></span>
-                        </h1>
-                    </div>
-                    <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                        <NavItem to="/profile" icon={<Home className="h-5 w-5" />}>
-                            Home
-                        </NavItem>
-                        <NavItem to="/exercises" icon={<Dumbbell className="h-5 w-5" />}>
-                            Exercises
-                        </NavItem>
-                        <NavItem to="/history" icon={<Calendar className="h-5 w-5" />}>
-                            History
-                        </NavItem>
-                    </div>
-                    <div className="flex items-center">
-                        <ModeToggle></ModeToggle>
-                        <Button variant="ghost" size="icon">
-                            <Settings className="h-5 w-5" />
-                        </Button>
-                        <Avatar className="ml-3">
-                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
+        <>
+            {/* for screens bigger than sm */}
+            <nav className="bg-background border-b sm:block hidden">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex justify-between h-16">
+                        <div className="flex">
+                            <h1 className="text-2xl font-bold flex items-center space-x-2">
+                                <Dumbbell className="h-6 w-6 text-primary" /> {/* Adjust size with `h-6 w-6` classes */}
+                                <span><Link to="/">GainsBook</Link></span>
+                            </h1>
+                        </div>
+                        <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                            <NavItem to="/profile" icon={<Home className="h-5 w-5" />}>
+                                Home
+                            </NavItem>
+                            <NavItem to="/exercises" icon={<Dumbbell className="h-5 w-5" />}>
+                                Exercises
+                            </NavItem>
+                            <NavItem to="/history" icon={<Calendar className="h-5 w-5" />}>
+                                History
+                            </NavItem>
+                        </div>
+                        <div className="flex items-center">
+                            <ModeToggle></ModeToggle>
+                            <Button variant="ghost" size="icon">
+                                <Settings className="h-5 w-5" />
+                            </Button>
+                            <Avatar className="ml-3">
+                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                        </div>
                     </div>
                 </div>
+            </nav>
+            {/* Mobile Navbar (Bottom) */}
+            <nav className="bg-background fixed bottom-0 left-0 right-0 sm:hidden flex justify-around items-center border-t z-10 p-2.5">
+                <NavItem to="/profile" icon={<Home className="h-6 w-6" />}>
+                    Home
+                </NavItem>
+                <NavItem to="/exercises" icon={<Dumbbell className="h-6 w-6" />}>
+                    Exercises
+                </NavItem>
+                <NavItem to="/history" icon={<Calendar className="h-6 w-6" />}>
+                    History
+                </NavItem>
+                <NavItem to="/profile" icon={<Avatar className="h-6 w-6">
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>}>
+                    Profile
+                </NavItem>
+
+            </nav>
+
+            {/* Mobile ModeToggle (Floating Top) */}
+            <div className="fixed top-2 right-2 sm:hidden z-20  rounded-full bg-popover" >
+
+                <ModeToggle />
+
+
             </div>
-        </nav>
+        </>
+
+
+
+
     );
 }
 
@@ -46,10 +80,10 @@ function NavItem({ to, icon, children }: { to: string; icon: React.ReactNode; ch
     return (
         <Link
             to={to}
-            className="inline-flex items-center px-1 pt-1 text-sm font-medium text-foreground hover:text-primary"
+            className="inline-flex flex-col items-center px-1 pt-1 text-sm font-medium text-foreground hover:text-primary sm:flex-row"
         >
             {icon}
-            <span className="ml-2">{children}</span>
+            <span className="text-xs mt-1 sm:ml-2">{children}</span>
         </Link>
     );
 }
