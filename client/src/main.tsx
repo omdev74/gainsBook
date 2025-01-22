2d5d780fd7068075b47ae1b298fbefc6c67fa4bf
Om dev <43951418+omdev74@users.noreply.github.com>
2025-01-22 13:58:24 -0500

    pwa config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,9 +6,19 @@ import App from './App.tsx';
 import { ThemeProvider } from './components/ThemeProvider.tsx';
 import { BrowserRouter } from 'react-router';
 import { AuthProvider } from './contexts/AuthContext.tsx';
-
+import { registerSW } from 'virtual:pwa-register';
 
 console.log(App);
+
+// Register the service worker with automatic updates
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('A new version of the app is available.');
+  },
+  onOfflineReady() {
+    console.log('The app is ready to work offline.');
+  },
+});
 // Rendering both components inside StrictMode
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,59 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from "vite-plugin-svgr";
 
 
 console.log("Resolved @shared path:", path.resolve(__dirname, "../packages/shared"));
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), VitePWA({
+    registerType: 'autoUpdate', // Automatically update the service worker
+    manifest: {
+      name: 'GainsBook',
+      short_name: 'GainsBook',
+      description: 'A Workout logger , where lives all your progress',
+      theme_color: '#FFFFFF',
+      icons: [
+        {
+          "src": "icons/manifest-icon-192.maskable.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "icons/manifest-icon-192.maskable.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "icons/manifest-icon-512.maskable.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "icons/manifest-icon-512.maskable.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ],
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/.*\.(?:png|jpg|jpeg|svg|js|css|html)$/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'assets-cache',
+          },
+        },
+      ],
+    },
+  }),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
